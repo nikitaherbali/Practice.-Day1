431bb19c435876b6e18e7ee5ba97bff2d6a0ca59
nikitaherbali <67627542+nikitaherbali@users.noreply.github.com>
2020-06-30 19:08:12 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-ctx.beginPath();       
-ctx.moveTo(30, 50);    
-ctx.lineTo(150, 100);
-ctx.lineWidth = 15;  
-ctx.stroke(); 
